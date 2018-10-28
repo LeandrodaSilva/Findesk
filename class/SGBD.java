75b3fd32d9804d38cca8f26f0eda9009fb5dd5d6
Leandro da Silva <ld_silva13@hotmail.com>
2018-10-27 21:12:57 -0300

    Add java classes

---
+++ class/SGBD.java
@@ -0,0 +1,21 @@
+package Diagrama de Classe;
+
+public class SGBD {
+
+	public void inserirItemBd() {
+
+	}
+
+	public void alterarItemBd() {
+
+	}
+
+	public void excluirItemBd() {
+
+	}
+
+	public void consultarItemBd() {
+
+	}
+
+}
