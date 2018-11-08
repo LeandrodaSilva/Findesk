ce2e52ca03cd7a23e824c9221522947eae1f36c6
Leandro da Silva <ld_silva13@hotmail.com>
2018-11-08 17:44:07 -0200

    ADD

---
+++ findesk.admnistrador/src/control/Administrador.java
@@ -0,0 +1,22 @@
+package control;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ld_si
+ */
+public class Administrador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
